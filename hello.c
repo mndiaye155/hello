196ab2842c47bfd5bfe17f59e28f4142c6206009
Moussa <mndiaye3@huskers.unl.edu>
2021-08-26 23:55:58 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Moussa Ndiaye
+ *         mndiaye3@huskers.unl.edu
+ * Date: 2021/08/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Moussa Ndiaye!!!\n");
+  printf("Computer Engineering\n");
+  
+  return 0;
+}
